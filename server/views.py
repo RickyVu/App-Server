781cc36a0bcfa1a38d3d9a68f422e8342cbb77e8
RickyVu <rickyvu189@gmail.com>
2023-05-13 15:52:43 +0800

    Updated previous version
    New:
    - Because Django does not create session automatically, I have to manually create one

--- server/views.py
+++ server/views.py
@@ -2,5 +2,7 @@ from django.middleware import csrf
 from django.http import HttpResponse, JsonResponse
 
 def initiate(request):
+    if not request.session.session_key:
+        request.session.create()
     session_id = request.session.session_key
     return JsonResponse({"session_id": session_id})
